0c3a601a59cc6776006629c9726b8f442ad642c4
Mathis Holler <m.holler@orange.fr>
2025-02-04 10:27:34 +0100

    Version 1

---
+++ crêpe.js
@@ -0,0 +1,19 @@
+
+function preparerPate(farine, lait, oeuf, sucre) { 
+    console.log(`Pâte prête avec ${farine} g ${lait} ml ${oeuf} oeuf ${sucre} g`)
+
+}
+
+let farine = 500
+let lait = 250
+let oeuf = 6
+let sucre = 150 
+
+preparerPate(farine, lait, oeuf, sucre)
+
+
+function cuirecrepe(farine, lait, oeuf, sucre){
+    console.log(`Crêpe cuite avec ${farine} g ${lait} ml ${oeuf} oeuf ${sucre} g`) 
+}
+
+cuirecrepe(farine,lait,oeuf,sucre)
